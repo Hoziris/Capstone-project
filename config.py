733217e384d9b54c4d85d4b101e799dd1b214d61
Iris AUBE <iris.aube@sciencespo.fr>
2025-02-01 11:45:44 +0100

    add first test version

---
+++ config.py
@@ -0,0 +1,27 @@
+#PARAMETERS TO FILL WITH CHARACTERISTICS AND TYPOLOGIES
+
+# EXPOSURE PARAMETERS
+
+he = 1 # external water depth (m)
+v = 0.5 # velocity (m/s)
+s = 0.05 # sediment concentration
+d = 24 # flood duration (h)
+q = 1 # presence of pollutants (1=oui, 0= non)
+
+# BUILDING PARAMETERS
+
+# Geometry
+FA = 100 # Footprint Area (m2)
+IH = 3 # Interstorey Height (m)
+BH = 2.8 # Basement Height (m)
+WT = 0.4 # Walls thickness (m)
+GL = 0.1 # GroundFloor Level (m)
+NF = 2 # Number of floors
+BT = 1     # Building type: 1- Detached, 2- Semi-detached, 3- Adjacent
+
+# Materials and equipments
+BS = 2     # Building structure: 1- Reinforced concrete, 2- Masonry, 3- Wood
+PD = 1     # Plant distribution: 1- Centralized, 2- Distributed
+PT = 1	    # Heating system type: 1- Radiator, 2- Underfloor heating
+YY = 1994  # Year of construction
+YR = 1994 # Year of last major renovation
