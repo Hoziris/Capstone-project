47972ad83c78ddcd4ee82305cd8f044b6d02fc75
Iris AUBE <iris.aube@sciencespo.fr>
2025-05-23 10:31:55 +0200

    Parameters of exposure and vulnerability
    Exposure : 6 different scenarios
    - FloodTest : values filled to test the model
    - FloodScenario : values for generating graphs
    - Runoff : flooding by runoff (ruissellement)
    - Quick Overflowing : flooding by quick overflowing (débordement rapide)
    - Slow Overflowing : flooding by quick overflowing (débordement lent)
    - Rising Groundwater : flooding by rising groundwater (remontée de nappes)
    
    Typology : dictionary with the geometric and material characteristics of the building
    Add of new parameters (Ad - Area of one dwelling, Sp - Structure of pavement)

--- config.py
+++ config.py
@@ -4,27 +4,31 @@
 
 exposure = {
     "FloodTest" : {
-    "He" : 1.5, # external water depth (m)
-    "ve" : 5, #velocity (m/s)
+    "He" : 0.5, # external water depth (m)
+    "ve" : 0.5, #velocity (m/s)
     "se" : 0.1, # sediment concentration
-    "d" : 24, # flood duration (h)
+    "d" : 48, # flood duration (h)
     "q" : 1 # presence of pollutants (1=oui, 0= non)
     },
 
     "FloodScenario" : {
-        "He" : None, "ve" : 3, "se" : 0.2, "d" : None, "q" : 1
+        "He" : None, "ve" : 0.2, "se" : 0.05, "d" : None, "q" : 1
     },
 
-    "Ruissellement" : {
-        "He" : 2.5, "ve" : 3, "se" : 0.2, "d" : 12, "q" : 1
+    "Runoff" : {
+        "He" : 2, "ve" : 1, "se" : 0.1, "d" : 12, "q" : 1
     },
 
-    "Débordement" : {
-        "He" : 1, "ve" : 0.5, "se" : 0.05, "d" : 24, "q" : 1
+    "Quick Overflowing" : {
+        "He" : 1, "ve" : 2, "se" : 0.1, "d" : 3, "q" : 1
     },
 
-    "Remontée de nappes" : {
-        "He" : 0.5, "ve" : 0, "se" : 0, "d" : 100, "q" : 0
+    "Slow Overflowing" : {
+        "He" : 1, "ve" : 0.2, "se" : 0.05, "d" : 72, "q" : 1
+    },
+
+    "Rising Groundwater" : {
+        "He" : 0.3, "ve" : 0, "se" : 0, "d" : 100, "q" : 0
     }
 }
 
@@ -32,25 +36,33 @@ exposure = {
 
 typology = {
     "BuildingTest" : {
-    # Geometry
-    "Sh" : 1, # Structure of housing (1 for collective housing, 0 for individual housing)
-    "Ai" : 50, # Internal Area (m2)
-    "Tw" : 0.4, # Thickness of walls (m)
-    "Nf" : 1, # Number of floors (including ground-floor)
+
+    #Geometry
+    "Sh" : 0, # Structure of housing (1 for collective housing, 0 for individual housing)
+    "Sj" : 2, # Structure juxtaposition (2 when 2 sides exposed in adjoined buildings, 4 when isolated building)
+    "Ai" : 100, # Internal Area (m2)
+    "Ad" : 100, #Area of one Dwelling (m2)
+    "Nf" : 2, # Number of floors (including ground-floor)
     "Hf" : 2.5, # Height of floor (m)
     "Hb" : 0, # Height of basement (m)
-    "Hg" : 1.5, # Height of ground (m)
-    "Sj" : 2, # Structure juxtaposition (2 when 2 sides exposed in adjoined buildings, 4 when isolated building)
+    "Hg" : 0, # Height of ground (m)
 
     # Materials and equipments
     "Sb" : 1,     # Structure of building: 1- Masonry, 2- Reinforced concrete, 3- Stone
+    "Sp" : 2, # Structure of Pavement: 1- Wood/Ceramic, 2-Porcelain Stoneware, 3-Marble/Natural Materials
     "YY" : 1960,  # Year of construction
     "YR" : 2000, # Year of last major renovation
+    "Tw": 0.4,  # Thickness of walls (m)
+    "Db" : 0, #Distribution of boiler : 0- centralized, 1- Distributed
     "Eb" : 0, # Existence of basement : 0- no, 1- yes
     "Ee" : 0, # Existence of elevator : 0- no, 1- yes
-    "Db" : 0, #Distribution of boiler : 0- centralized, 1- Distributed
-    "Hcs" : 0, # Height of crawl space
-    "Er" : 0 # existence of electrical radiators : 0- No, 1- yes
+    "Er" : 0, # existence of electrical radiators : 0- No, 1- yes
+    "Hcs": 0, # Height of crawl space
 
+    },
+
+    "LC1" : {
+        "Sh" : 1, "Sj" : 2, "Ai" : 100, "Ad" : 0, "Nf" : 1, "Hf" : 2.5, "Hb" : 2, "Hg" : 0.1,
+        "Sb" : 1, "Sp" : 1, "YY" : 1990, "Yr" : 2000, "Tw" : 0.4, "Db" : 1, "Eb" : 1, "Ee" : 0, "Er" : 0, "Hcs" : 0
     }
 }
