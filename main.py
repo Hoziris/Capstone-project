ce88c79c947707024dcf4f74cf922fd184d610a7
Iris AUBE <iris.aube@sciencespo.fr>
2025-05-23 10:33:58 +0200

    Main
    Choice of types of flood and building to call the total cost function (DamageCalculation) and print a camembert of costs

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from main_function import DamageCalculation, print_camembert
 
-
-
 if __name__ == "__main__":
     # Defining the type of flood and type of building
-    flood_type = "FloodTest"
+    flood_type = "Rising Groundwater"
     building_type = "BuildingTest"
 
     # Call of calculation of damages function
     try:
         costs = DamageCalculation(flood_type, building_type)[1]
         print(costs)
-        print_camembert(costs)
+        print_camembert(costs, flood_type)
     except ValueError as e:
         print(f"Error : {e}")
