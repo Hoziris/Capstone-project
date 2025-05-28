5b8fc0755d89a974512999bba5a60f48c1f719db
Iris AUBE <iris.aube@sciencespo.fr>
2025-05-28 19:15:53 +0200

    Creates a matrix of costs according to height and duration of flood

--- flood_scenario.py
+++ flood_scenario.py
@@ -1,6 +1,7 @@
 
 from main_function import DamageCalculation
-from config import exposure, typology
+from config import user, exposure
+from attributing_typology import complete_user
 import numpy as np
 import pandas as pd
 
@@ -10,12 +11,13 @@ if __name__ == "__main__":
     duration = np.arange(0, 145, 12)
     height = np.arange(0, 91, 10)
     matrix = np.full((len(height), len(duration)), np.nan)
+    user_filled = complete_user(user)
 
     for i in duration:
         for j in height:
             exposure["FloodScenario"]["d"] = i
             exposure["FloodScenario"]["He"] = j/100
-            matrix[int(j / 10), int(i / 12)] = DamageCalculation("FloodScenario", "BuildingTest")[0]
+            matrix[int(j / 10), int(i / 12)] = DamageCalculation("FloodScenario", user_filled)[0]
 
 df = pd.DataFrame(matrix, index=height, columns=duration)
 print(df)
