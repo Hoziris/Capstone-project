3c310ddf2e962119a9c2b14077e34a8b756fe037
Iris AUBE <iris.aube@sciencespo.fr>
2025-05-28 19:11:55 +0200

    Dictionary of prices
    Takes into account Quality of Construction Qc
    Changes in preliminary measures cost

--- prices.py
+++ prices.py
@@ -1,9 +1,9 @@
-from config import typology
+from config import user
 
 catalogue = {
-    "Pp1" : 20, # pumping [€/m3]
-    "Pp2" : 35, # waste disposal [€/m3]
-    "Pp3" : 2.4, # cleaning [€/m2]
+    "Pp1" : 5, # pumping [€/m3]
+    "Pp2" : 200, # waste disposal [€/m3]
+    "Pp3" : 8, # cleaning [€/m2] 3 ?
     "Pp4" : 18, # dehumidification [€/m3]
 
     "Pc1a" : {"low" : 99, "medium" : 99, "high" : 141}, # wood pavement [€/m2]
@@ -21,14 +21,14 @@ catalogue = {
     "Pc8c": {"low" : 632, "medium" : 763, "high" : 800},  # entrance lock door [€/unit]
     "Pc9a" : {"low" : 595, "medium" : 780, "high" : 1276}, # window [€/unit]
     "Pc9b" : {"low" : 867, "medium" : 1055, "high" : 1447}, # window bay [€/unit]
-    "Pc10a" : (15300 + (typology["BuildingTest"]["Nf"]-1) * 2000) * typology["BuildingTest"]["Sh"] + 9000 * (1-typology["BuildingTest"]["Sh"]), # boiler centralized [€/unit]
+    "Pc10a" : (15300 + (user["Nf"]-1) * 2000) * user["Sh"] + 4500 * (1-user["Sh"]), # boiler centralized [€/unit]
     "Pc10b" : 4500, # boiler distributed [€/unit]
     "Pc11" : {"low" : 15, "medium" : 31, "high" : 36}, # internal painting [€/m2]
     "Pc12" : {"low" : 27, "medium" : 51, "high" : 51}, # external painting [€/m2]
-    "Pc13" : {"low" : 5000, "medium" : 12500, "high" : 20000}, # elevator [€/unit]
-    "Pc14a" : 100 * int(typology["BuildingTest"]["YR"]<1990) + 250 * int(typology["BuildingTest"]["YR"]>=1990), # electric radiator [€/unit]
+    "Pc13" : {"low" : 500, "medium" : 1250, "high" : 2000}, # elevator [€/unit]
+    "Pc14a" : 100 * int(user["Yc"]<1990) + 250 * int(user["Yc"]>=1990), # electric radiator [€/unit]
     "Pc14b" : 50, # radiator purging [€/unit]
-    "Pc14c" : 10 * typology["BuildingTest"]["Ai"], # radiator mud pulled out [€/house]
+    "Pc14c" : 10 * user["Ai"], # radiator mud pulled out [€/house]
     "Pc15a" : 100, # electrical system renovation [€/m2]
     "Pc15b" : {"low" : 0, "medium" : 500, "high" : 5500},# alarm system [€/unit]
     "Pc16" : 100, # plumbing system renovation [€/m2]
