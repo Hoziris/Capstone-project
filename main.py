2cbca7ec46b599f3a7f920426d2965baf2d7512e
Iris AUBE <iris.aube@sciencespo.fr>
2025-05-28 19:18:54 +0200

    Main
    Changes due to integration of adaptation measures

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from main_function import DamageCalculation, print_camembert
+from config import user
+from attributing_typology import complete_user
+from AdaptationCost import adaptation_cost
 
 if __name__ == "__main__":
+
     # Defining the type of flood and type of building
-    flood_type = "Rising Groundwater"
-    building_type = "BuildingTest"
+    flood_type = "Runoff" # FloodTest, Runoff, Quick Overflowing, Slow Overflowing, Rising Groundwater
+    adapt_package = "Advanced" # None, Basic, Medium, Advanced or Pro
 
     # Call of calculation of damages function
     try:
-        costs = DamageCalculation(flood_type, building_type)[1]
-        print(costs)
-        print_camembert(costs, flood_type)
+        user_filled = complete_user(user)
+        ca = adaptation_cost(user_filled, adapt_package)
+        print("Adaptation measures cost", ca)
+        s, costs_none = DamageCalculation(flood_type, user_filled, "None")
+        t, costs_adapt = DamageCalculation(flood_type, user_filled, adapt_package)
+        print("none", costs_none)
+        print("advanced", costs_adapt)
+        #print_camembert(costs, flood_type)
     except ValueError as e:
         print(f"Error : {e}")
